f7fedee9bf5bd83481710c1cca4185785bbd2d0b
eMobilis <alphoncerotich30@gmail.com>
2023-02-21 15:22:08 +0000

    my commit

---
+++ Exercise.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter Any Number"))
+num2 = int(input("Enter a second number"))
+
+
+
+
+
+print(num1)
+
+print(num2)
---
+++ Variables.py
@@ -0,0 +1,25 @@
+name = "Alkid"
+age = "20 yrs"
+print(f"my name is {name} and i'm {age} years")
+
+school = "eMobilis"
+course = "full stuck"
+print(f"I study at {school}, pursuing {course}")
+
+place = "Nairobi"
+print(place)
+
+Gender = "male"
+print(Gender)
+
+located = "Westland"
+center = "Kipro center, 3rd floor"
+print(f"The school is located at {located}, {center}")
+
+time = "3 hrs"
+print(time)
+
+
+
+
+
