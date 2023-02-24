c3e09803b13d645ad4892553c80aed87ab6feb54
eMobilis <alphoncerotich30@gmail.com>
2023-02-24 14:32:53 +0000

    first commit

--- Exercise-five.py
+++ Exercise-five.py
@@ -1,2 +1,13 @@
-import datetime
-day_of_the week datetime
+day_of_week = input("Enter the day of the week  ")
+if day_of_week == "monday":
+    print("false")
+elif day_of_week == "tuesday":
+    print("false")
+elif day_of_week == "wednesday":
+    print("false")
+elif day_of_week == "thursday":
+    print("true")
+elif day_of_week == "friday":
+    print("false")
+else:
+    print('Try again')
