56b081bc6b22be32a2fc1dc9407c4a047d027307
eMobilis <alphoncerotich30@gmail.com>
2023-02-24 15:53:46 +0000

    first commit

---
+++ Function_Exercise.py
@@ -0,0 +1,5 @@
+def calculate_months():
+    age = int(input("Enter the age  "))
+    months = age * 12
+    print(f"you have lived for {months} months")
+calculate_months()
--- Function_with_Parametors.py
+++ Function_with_Parametors.py
@@ -1,10 +1,6 @@
 def producttwonums(num1, num2):
     results = num1 * num2
     print(results)
-producttwonums(50, 10)
+producttwonums(500, 10)
 
 
-def dividetwonums(num1, num3):
-    results = num1 / num3
-    print(results)
-dividetwonums(60, 25)
