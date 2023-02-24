1f48d63c402a6c8b243adfa2f5f8ffbc774c0ba6
eMobilis <alphoncerotich30@gmail.com>
2023-02-24 15:10:19 +0000

    first commit

---
+++ Function_with_Parametors.py
@@ -0,0 +1,10 @@
+def producttwonums(num1, num2):
+    results = num1 * num2
+    print(results)
+producttwonums(50, 10)
+
+
+def dividetwonums(num1, num3):
+    results = num1 / num3
+    print(results)
+dividetwonums(60, 25)
---
+++ Functions.py
@@ -0,0 +1,15 @@
+def greetings():
+    print("Hello Kioko")
+greetings()
+
+def feedback():
+    print("Hello too")
+feedback()
+
+def greetings():
+    print("I will visit you tomorrow")
+greetings()
+
+def feedback():
+    print("You are highly welcome Kimani")
+feedback()
